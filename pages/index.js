7d046de75d3ad4b44c84076d33defa816000917a
Nico Järvinen <nico.k.jarvinen@gmail.com>
2021-04-24 17:53:14 +0300

    added arrow pointing down on mobile

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,22 @@ export default function Home() {
           <img className="" src="/static/images/profileDraw.svg" alt="/" />
         </div>
         <div className="self-end mt-24 h-10 w-10 block md:hidden">
-          <svg
-            className="animate-bounce"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              strokeWidth="2"
-              d="M19 14l-7 7m0 0l-7-7m7 7V3"
-            />
-          </svg>
+          <a href="#about">
+            <svg
+              className="animate-bounce"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                strokeWidth="2"
+                d="M19 14l-7 7m0 0l-7-7m7 7V3"
+              />
+            </svg>
+          </a>
         </div>
       </div>
 
